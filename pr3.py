d8c5ba44d254d45b1d39ec3e216c3f7282610281
sumeetby16 <sumeetby16>
2025-10-23 15:14:16 +0530

    farnanate

--- pr3.py
+++ pr3.py
@@ -10,3 +10,19 @@ def check_temperature(temp_celsius):
 
 
 
+
+
+def check_temperature(temp_celsius):
+    if temp_celsius < 15:
+        status = "Cold"
+    elif 15 <= temp_celsius <= 30:
+        status = "Normal"
+    else:
+        status = "Hot"
+
+    temp_fahrenheit = (temp_celsius * 9/5) + 32
+    print(f"Temperature: {temp_celsius}°C")
+    print(f"Status: {status}")
+    print(f"Temperature in Fahrenheit: {temp_fahrenheit:.1f}°F")
+
+check_temperature(35)
